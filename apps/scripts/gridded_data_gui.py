d9732a65cbcf11b1fc29911027116ed7ef5c3025
Justine Missik <missik.2@osu.edu>
2022-10-31 21:11:33 -0400

    Updates to gridded dataset GUI - Fix alignment of buttons - Move components to cards - Add function to save dataset

--- apps/scripts/gridded_data_gui.py
+++ apps/scripts/gridded_data_gui.py
@@ -50,12 +50,12 @@ class GriddedDataExplorer(param.Parameterized):
 
     # TODO replace with new file selector 
     filein = param_widget(pn.widgets.TextInput(placeholder='Select a file...', name='Data file'))
-    load_data_button = param_widget(pn.widgets.Button(button_type='primary', name='Load data'))
+    load_data_button = param_widget(pn.widgets.Button(button_type='primary', name='Load data', align='end'))
     timevar = param_widget(pn.widgets.Select(options=[], name='Time'))
     latvar = param_widget(pn.widgets.Select(options=[], name='Latitude'))
     lonvar = param_widget(pn.widgets.Select(options=[], name='Longitude'))
     zvar = param_widget(pn.widgets.Select(options=[], name='Variable of interest'))
-    update_varnames = param_widget(pn.widgets.Button(button_type='primary', name='Update variable names'))
+    update_varnames = param_widget(pn.widgets.Button(button_type='primary', name='Update variable names', align='end'))
 
     polyfile = param_widget(pn.widgets.TextInput(placeholder='Select a file...', name='Polygon file'))
     load_polyfile = param_widget(pn.widgets.Button(button_type='primary', name='Load data'))
@@ -74,9 +74,13 @@ class GriddedDataExplorer(param.Parameterized):
     poly_selection_dict = {'Select within boundary':False, 'Mask within boundary':True}
     selection_type = param_widget(pn.widgets.RadioBoxGroup(name='Selection options', options = poly_selection_dict))
     
-    update_filters = param_widget(pn.widgets.Button(button_type='primary', name='Update filters'))
-    revert_filters = param_widget(pn.widgets.Button(button_type='primary', name='Revert filters'))
+    update_filters = param_widget(pn.widgets.Button(button_type='primary', name='Update filters', align='end'))
+    revert_filters = param_widget(pn.widgets.Button(button_type='primary', name='Revert filters', align='end'))
 
+    # Save dataset
+    output_fname = param_widget(pn.widgets.TextInput(placeholder='Output filename...', 
+                                                     value='processed_dataset.nc',name='Output file'))
+    save_ds = param_widget(pn.widgets.Button(name='Save dataset', button_type='primary', align='end'))
 
     def __init__(self, **params):
         super().__init__(**params)
@@ -96,35 +100,43 @@ class GriddedDataExplorer(param.Parameterized):
         self.selection_type.name = 'Polygon selection options'
         self.update_filters.name = 'Update filters'
         self.revert_filters.name = 'Revert filters'
+        self.output_fname.name = 'Output file'
+        self.save_ds.name = "Save dataset"
                
+        # Widget groups
         self.filein_widgets = pn.Row(self.filein, self.load_data_button)
-        self.varname_widgets = pn.Row(self.timevar, self.latvar, self.lonvar, self.zvar, self.update_varnames)
+        self.varname_widgets = pn.Row(self.timevar, self.latvar, self.lonvar, self.zvar, self.update_varnames)       
+        self.file_input_card = pn.Card(self.filein_widgets, self.varname_widgets, 
+                                       title="Input environmental dataset file")
 
-        self.selection_options = pn.WidgetBox(self.date_range, self.selection_type, 
-                                              pn.Row(self.update_filters, self.revert_filters))
-        self.polyfile_widgets = pn.Row(self.polyfile, self.load_polyfile)
+        self.selection_options = pn.Card(self.date_range, self.selection_type, 
+                                              pn.Row(self.update_filters, self.revert_filters), title="Selection options")
+        self.polyfile_widgets = pn.Card(self.polyfile, self.load_polyfile, title='Input polygon file')
         
+        self.outfile_widgets = pn.Card(pn.Row(self.output_fname, self.save_ds))
         #Add view
         self.view = pn.Column(
             pn.pane.Markdown("## Select file to plot!"),
-            self.filein_widgets, 
-            self.varname_widgets,
+            self.file_input_card,
             self.polyfile_widgets,
             self.selection_options,
-            pn.pane.Alert(self.status_text)           
+            self.outfile_widgets,
+            pn.Card(pn.pane.Alert(self.status_text), title='Status')           
             )
 
-    @param.depends("ds_raw", watch=True)
+    @param.depends("update_varnames.value", watch=True)
     def update_selection_widgets(self):
         if self.ds_raw is not None:
             self.status_text = 'Updating widgets'
             self.date_range = pn.widgets.DateRangeSlider(name="Date range selection", 
-                                                         start=self.ds_raw.time.min().values, 
-                                                         end=self.ds_raw.time.max().values)
+                                                         start=self.ds_raw[self.timevar.value].min().values, 
+                                                         end=self.ds_raw[self.timevar.value].max().values)
             
-            self.selection_options = pn.WidgetBox(self.date_range, self.selection_type, 
-                                                  pn.Row(self.update_filters, self.revert_filters))
-            self.view[4] = self.selection_options
+            # self.selection_options = pn.WidgetBox(self.date_range, self.selection_type, 
+            #                                       pn.Row(self.update_filters, self.revert_filters))
+            self.selection_options = pn.Card(self.date_range, self.selection_type, 
+                                              pn.Row(self.update_filters, self.revert_filters), title="Selection options")
+            self.view[3] = self.selection_options
           
     @param.depends("load_data_button.value", watch=True)
     def load_data(self):
@@ -180,21 +192,23 @@ class GriddedDataExplorer(param.Parameterized):
             
     @param.depends("status_text", watch=True)
     def update_status_view(self):
-        self.view[-1] = pn.pane.Alert(self.status_text)
+        self.view[-1] = pn.Card(pn.pane.Alert(self.status_text), title='Status')
 
     @param.depends("ds", "update_varnames.value", "poly", watch=True)
     def update_plot_view(self):
         if all([self.timevar.value, self.latvar.value, self.lonvar.value, self.zvar.value]):
             self.status_text = "Creating plot"
             width = 500
-            ds_plot = plot_gridded_data(self.ds).opts(frame_width=width)
+            ds_plot = plot_gridded_data(self.ds, x=self.lonvar.value, y=self.latvar.value, z=self.zvar.value, 
+                                        time=self.timevar.value).opts(frame_width=width)
             if self.poly is not None:
                 ds_plot = ds_plot * gv.Polygons(self.poly).opts(fill_color=None)
             plot = pn.pane.HoloViews(ds_plot)
             widget = plot.widget_box.objects[0]
             widget.width = width
             widget.align = 'center'
-            ts_plot = plot_avg_timeseries(self.ds).opts(frame_width=width)
+            ts_plot = plot_avg_timeseries(self.ds, x=self.lonvar.value, y=self.latvar.value, 
+                                          z=self.zvar.value, time=self.timevar.value ).opts(frame_width=width)
             figs_with_widget = pn.Row(plot, pn.Column(widget,ts_plot, align='center'), pn.Column(self.ds, background='whitesmoke'))
             
             self.status_text = "Plot created!"
@@ -202,6 +216,13 @@ class GriddedDataExplorer(param.Parameterized):
             self.view[0] = figs_with_widget
         else:
             self.status_text = 'Please specify variable names'
+    
+    @param.depends("save_ds.value", watch=True)
+    def save_dataset(self):
+        outfile = Path(self.output_fname.value).resolve()
+        self.ds.to_netcdf(outfile)
+        self.status_text = f'File saved to: {outfile}'
+            
 
 # %%
 g = GriddedDataExplorer()
