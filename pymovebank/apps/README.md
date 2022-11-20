5535c8a0f5fc3d624afca56ab0a93d8769ff1c3d
Madeline Scyphers <madelinescyphers@gmail.com>
2022-11-20 12:30:06 -0500

    Update env files to remove jstree

--- pymovebank/apps/README.md
+++ pymovebank/apps/README.md
@@ -1,5 +1,5 @@
 to launch apps, from root of directory:
 
 ```bash
-python -m panel serve pymovebank/apps/apps/*.py
+python -m panel serve pymovebank/apps/apps/*.py --glob
 ```
--- scripts/post_install.bat
+++ scripts/post_install.bat
@@ -1,6 +1,6 @@
 source activate "${PREFIX}"
 
-pip install git+https://github.com/jemissik/pymovebank.git@feature/conda-constructor awesome-panel-extensions==20221019.2 git+https://github.com/madeline-scyphers/panel-jstree.git
+pip install git+https://github.com/jemissik/pymovebank.git@feature/conda-constructor awesome-panel-extensions==20221019.2
 
 pmv_path=`python -c "import pymovebank; print(pymovebank.__path__[0])"`
 
@@ -9,5 +9,5 @@ source activate ${PREFIX}
 
 #python -mwebbrowser http://localhost:5006/home
 
-python -m panel serve ${pmv_path}/apps/apps/*.py
+python -m panel serve ${pmv_path}/apps/apps/*.py --glob
 " > "${PREFIX}"/app.bat
