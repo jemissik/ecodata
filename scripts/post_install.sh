9b18663dd93fd98969e5a592053a1ca98d799c6c
Madeline Scyphers <madelinescyphers@gmail.com>
2022-11-19 23:45:50 -0500

    changing setup.cfg to use find: for packages and add jstree to post_install

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -7,7 +7,7 @@ echo 'installing form github'
 source activate "$PREFIX"
 
 which python
-pip install git+https://github.com/jemissik/pymovebank.git@feature/conda-constructor awesome-panel-extensions==20221019.2
+pip install git+https://github.com/jemissik/pymovebank.git@feature/conda-constructor awesome-panel-extensions==20221019.2 git+https://github.com/madeline-scyphers/panel-jstree.git
 
 
 pmv_path=`python -c "import pymovebank; print(pymovebank.__path__[0])"`
