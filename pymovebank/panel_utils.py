299b00b38fcd933fdf685f1ce04ce17fdaa76c3e
Justine Missik <missik.2@osu.edu>
2022-11-22 02:25:15 -0700

    Add movie maker app

--- pymovebank/panel_utils.py
+++ pymovebank/panel_utils.py
@@ -2,8 +2,13 @@ import param
 import panel as pn
 import functools
 import logging
+import subprocess
+import os
+import shlex
+from pathlib import Path
 
 from tkinter import Tk, filedialog
+IS_WINDOWS = os.name == "nt"
 
 logger = logging.getLogger(__file__)
 
@@ -80,3 +85,22 @@ def try_catch(msg="Error... Check options and try again"):
                 self.status_text = msg
         return tru_dec
     return inner
+
+
+def split_shell_command(cmd: str):
+    """
+    split shell command for passing to python subproccess.
+    This should correctly split commands like "echo 'Hello, World!'"
+    to ['echo', 'Hello, World!'] (2 items) and not ['echo', "'Hello,", "World!'"] (3 items)
+    It also works for posix and windows systems appropriately
+    """
+    return shlex.split(cmd, posix=not IS_WINDOWS)
+
+
+def make_mp4_from_frames(frames_dir, output_file, frame_rate):
+    frames_pattern = Path(frames_dir) / 'Frame%d.png'
+    cmd = f"""ffmpeg -framerate {frame_rate} -i {frames_pattern}
+    -vf pad="width=ceil(iw/2)*2:height=ceil(ih/2)*2"
+    -c:v libx264 -pix_fmt yuv420p -y {output_file}"""
+
+    subprocess.run(split_shell_command(cmd))
