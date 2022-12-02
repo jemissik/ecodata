5a1576bb72413f2a8ec1760691d23a7c6bef512d
Justine Missik <missik.2@osu.edu>
2022-12-02 17:53:02 -0500

    update pages workflow and skip panel test for linux

--- pymovebank/tests/test_apps.py
+++ pymovebank/tests/test_apps.py
@@ -1,6 +1,8 @@
 import requests
+import pytest
+from sys import platform
 
-@pytest.mark.skip(reason="Needs update")
+@pytest.mark.skipif(platform == "linux" or platform == "linux2")
 def test_server_has_every_app(serve_apps, port, apps):
     for app in apps:
         r = requests.get(f"http://localhost:{port}/{app}")
