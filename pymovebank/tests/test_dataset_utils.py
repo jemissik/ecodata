9f8977fea89ee09df285fa95c7f6df03f2f58478
Justine Missik <missik.2@osu.edu>
2023-01-04 14:02:49 -0500

    Add test for installing test data bundle

--- pymovebank/tests/test_dataset_utils.py
+++ pymovebank/tests/test_dataset_utils.py
@@ -1,6 +1,32 @@
 import pymovebank
 import pytest
 
+def test_that_test_data_bundle_installs():
+
+    pymovebank.install_test_datasets()
+
+    # Files that should be in the test dataset
+    data_files = {
+        "ECMWF_public_caribou.nc",
+        "public_caribou_tracks.csv",
+        "NASA_public_caribou.nc",
+        "public_caribou_tracks_clipped.csv",
+        "public_caribou_lakes",
+        "public_caribou_tracks_extent.geojson",
+        "public_caribou_protected_areas",
+        "public_caribou_tracks_web.csv",
+        "public_caribou_ref.csv",
+        "public_caribou_roads"
+    }
+
+    assert data_files.issubset(pymovebank.available())
+
+def test_that_installing_test_bundle_works_if_already_installed():
+
+    # Run the install method twice
+    pymovebank.install_test_datasets()
+    pymovebank.install_test_datasets()
+
 @pytest.mark.skip(reason="Needs updating for new test datasets")
 def test_that_valid_dataset_returns_correct_path():
     # Given a valid dataset
