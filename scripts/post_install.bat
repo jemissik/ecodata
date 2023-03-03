74e5c69fb557eda4ba06f9e805753b2b5b579117
Madeline Scyphers <madelinescyphers@gmail.com>
2023-03-02 23:10:04 -0500

    Update constructor scripts for post conda-forge release
    ecodata is on condaforge now so now we just need to install ecodata
    from condaforge and everything should come alonge with it.
    Update construct.yaml to reflect this.
    Also remove old environment-clean-build.yml file and env lock files
    which are not needed anymore.

--- scripts/post_install.bat
+++ scripts/post_install.bat
@@ -1,7 +1,5 @@
 call "%PREFIX%\Scripts\activate.bat"
 
-for /f %%i in ('python -c "import ecodata; print(ecodata.__path__[0])"') do set eco_path=%%i
-
 echo|set /p="
 call %PREFIX%\Scripts\activate.bat && python -m ecodata.app
 " > "${PREFIX}"/app.bat
--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -3,6 +3,11 @@ set -ex
 source activate "${PREFIX}"
 
 echo "
+
+echo \"
+Launching App
+\"
+
 source activate ${PREFIX}; python -m ecodata.app
 " > "${PREFIX}"/app.sh
 chmod +x "${PREFIX}"/app.sh
