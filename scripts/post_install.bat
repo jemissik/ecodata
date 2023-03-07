374e3de943d57601806dc631a1fbf48c1f829f05
Madeline Scyphers <madelinescyphers@gmail.com>
2023-03-07 10:15:03 -0500

    Change outputted constructor script name from app.sh/bat to ecodata.sh/bat

--- scripts/post_install.bat
+++ scripts/post_install.bat
@@ -1,8 +1,7 @@
-robocopy "%PREFIX%\*.*" "%PREFIX%\pkg" /MOV /E
+Rem robocopy "%PREFIX%\*.*" "%PREFIX%\pkg" /MOV /E
 
 (
 echo echo Launching app...
 echo call %PREFIX%\lib\Scripts\activate.bat
 echo python -m ecodata.app
-)>%PREFIX%\app.bat
-pause
+)>%PREFIX%\ecodata.bat
--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -1,13 +1,13 @@
 set -ex
 
-source activate "${PREFIX}"
+#source activate "${PREFIX}"
 
 echo "
 
 echo \"
-Launching App
+Launching App...
 \"
 
 source activate ${PREFIX}; python -m ecodata.app
 " > "${PREFIX}"/app.sh
-chmod +x "${PREFIX}"/app.sh
+chmod +x "${PREFIX}"/ecodata.sh
