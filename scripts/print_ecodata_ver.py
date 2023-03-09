30a7b04d3291f70df8e75ec2a61a3645dd2b7a76
Madeline Scyphers <madelinescyphers@gmail.com>
2023-03-09 17:48:47 -0500

    Install all installer_types

--- scripts/print_ecodata_ver.py
+++ scripts/print_ecodata_ver.py
@@ -6,15 +6,7 @@ def main():
     output = subprocess.run("conda search conda-forge::ecodata".split(), capture_output=True, text=True)
     lines = output.stdout.splitlines()
     lastline = lines[-1]
-    version1 = lastline.split()[1]
-    output = subprocess.run("conda search cf-staging::ecodata".split(), capture_output=True, text=True)
-    lines = output.stdout.splitlines()
-    lastline = lines[-1]
-    version2 = lastline.split()[1]
-    if version1 >= version2:
-        version = version1
-    else:
-        version = version2
+    version = lastline.split()[1]
     print(version)
 
 
