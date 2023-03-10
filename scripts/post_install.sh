5a7705e490f6d7181491af9d0d46207777f185f5
Justine Missik <missik.2@osu.edu>
2023-03-09 20:04:57 -0500

    change launch script to be .command

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -8,10 +8,10 @@ Launching App...
 \"
 
 source activate ${PREFIX}; python -m ecodata.app
-" > "${PREFIX}"/ecodata.sh
-chmod +x "${PREFIX}"/ecodata.sh
+" > "${PREFIX}"/ecodata.command
+chmod +x "${PREFIX}"/ecodata.command
 
-cp "${PREFIX}"/ecodata.sh ~/Downloads/ecodata.sh
+cp "${PREFIX}"/ecodata.command ~/Downloads/ecodata.command
 
 
 echo "
@@ -22,5 +22,5 @@ Updating ECODATA
 
 source activate ${PREFIX}; conda update ecodata
 
-" > "${PREFIX}"/update_ecodata.sh
-chmod a+x "${PREFIX}"/update_ecodata.sh
+" > "${PREFIX}"/update_ecodata.command
+chmod a+x "${PREFIX}"/update_ecodata.command
