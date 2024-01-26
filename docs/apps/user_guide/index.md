635c6682588ff3f7d2a912ea1be4956fb1ab3f14
Jashnavi Bommana <91922122+bommana401@users.noreply.github.com>
2024-01-26 13:55:43 -0500

    add folder to extract time

--- docs/apps/user_guide/index.md
+++ docs/apps/user_guide/index.md
@@ -10,20 +10,23 @@ Want a downloadable version of the docs? This documentation is also [available f
 
 Before using this program, download a local copy of the tracking data you want to use, in Movebank’s .csv format. It is possible to subset or combine data from different studies. Read the [installation instructions](../installation) to install or update the program.
 
-To launch the apps, double-click on the launch file (``ecodata.command`` for Mac,
-``ecodata.bat`` for Windows). A terminal window will open, indicating that the apps are launching. When the apps finish launching, a window will open on your default web browser, showing the app gallery. The apps are running locally at localhost:5006. *Note: There may be a short wait (10+ seconds) before the browser window opens.*
+To launch the apps, double-click on the launch file (`ecodata.command` for Mac,
+`ecodata.bat` for Windows). A terminal window will open, indicating that the apps are launching. When the apps finish launching, a window will open on your default web browser, showing the app gallery. The apps are running locally at localhost:5006. _Note: There may be a short wait (10+ seconds) before the browser window opens._
 
 When it opens, it will display the main panel, showing the four apps. Click on an app to launch it. From there, you can navigate between apps within the interface, or switch between them by pasting these URLs in your browser window:
 
-* Main app gallery: <http://localhost:5006>
-* Tracks Explorer App: <http://localhost:5006/tracks_explorer_app>
-* Gridded Data Explorer App: <http://localhost:5006/gridded_data_explorer_app>
-* Subsetter App: <http://localhost:5006/subsetter_app>
-* Movie Maker App: <http://localhost:5006/movie_maker_app>
+- Main app gallery: <http://localhost:5006>
+- Tracks Explorer App: <http://localhost:5006/tracks_explorer_app>
+- Gridded Data Explorer App: <http://localhost:5006/gridded_data_explorer_app>
+- Subsetter App: <http://localhost:5006/subsetter_app>
+- Movie Maker App: <http://localhost:5006/movie_maker_app>
+<!-- added this line new -->
+- Time Extractor App: <http://localhost:5006/time_extractor_app>
 
 ![ecodata-prepare_panel](../images/ecodata-prepare_panel.png)
 
 ## Apps
+
 ```{toctree}
 ---
 maxdepth: 2
---
+++ ecodata/app/apps/time_extractor_app.py
@@ -0,0 +1,115 @@
+import logging
+from pathlib import Path
+
+import panel as pn
+import param
+from panel.io.loading import start_loading_spinner, stop_loading_spinner
+
+from ecodata.app.config import DEFAULT_TEMPLATE
+from ecodata.panel_utils import (
+    make_mp4_from_frames,
+    param_widget,
+    register_view,
+    try_catch, rename_param_widgets,
+)
+from ecodata.app.models import FileSelector
+
+import os
+import io
+import panel as pn
+pn.extension('tabulator', 'terminal', 'ipywidgets', sizing_mode='stretch_width', loading_spinner='dots')
+from tkinter import Tk, filedialog
+import ecodata as eco
+from ecodata.raster_utils import geotif2nc
+
+logger = logging.getLogger(__file__)
+
+
+class MovieMaker(param.Parameterized):
+
+    # Frames directory
+    frames_dir = param_widget(FileSelector(constrain_path=False, expanded=True))
+
+    # Output file
+    output_file = param_widget(
+        pn.widgets.TextInput(placeholder="Choose an output file...", value="output.mp4", name="Output file")
+    )
+
+    frame_rate = param_widget(
+        pn.widgets.EditableIntSlider(name="Frame rate", start=1, end=30, step=1, value=1, sizing_mode="fixed")
+    )
+
+    # Go button
+    go_button = param_widget(pn.widgets.Button(name="Make movie", button_type="primary", sizing_mode="fixed"))
+
+    # Status
+    status_text = param.String("Ready...")
+
+    def __init__(self, **params):
+        super().__init__(**params)
+
+        # Reset names for panel widgets
+        rename_param_widgets(
+            self,
+            [
+                "frames_dir",
+                "output_file",
+                "frame_rate",
+                "go_button",
+            ]
+        )
+
+        # Widget groups
+        self.movie_widgets = pn.Card(
+            self.frames_dir, self.frame_rate, self.output_file, self.go_button, title="Movie maker"
+        )
+
+        self.status = pn.pane.Alert(self.status_text)
+
+        # View
+        self.view_objects = {"movie_widgets": 0, "status": 1}
+
+        self.alert = pn.pane.Markdown(self.status_text)
+
+        self.view = pn.Column(self.movie_widgets)
+
+    @try_catch()
+    @param.depends("status_text", watch=True)
+    def update_status_text(self):
+        self.alert.object = self.status_text
+
+    @try_catch()
+    @param.depends("go_button.value", watch=True)
+    def make_movie(self):
+
+        self.status_text = "Creating movie..."
+        start_loading_spinner(self.view)
+        try:
+            output_file = make_mp4_from_frames(
+                self.frames_dir.value, self.output_file.value, frame_rate=self.frame_rate.value
+            )
+            assert output_file.exists()
+            self.status_text = f"Movie saved to: {output_file}"
+
+        except Exception as e:
+            msg = "Error creating movie...."
+            logger.warning(msg + f":\n{e!r}")
+            self.status_text = msg
+        finally:
+            stop_loading_spinner(self.view)
+
+
+@register_view()
+def view():
+    viewer = MovieMaker()
+    template = DEFAULT_TEMPLATE(
+        main=[viewer.alert, viewer.view],
+    )
+    return template
+
+
+if __name__ == "__main__":
+    pn.serve({Path(__file__).name: view})
+
+if __name__.startswith("bokeh"):
+    view()
