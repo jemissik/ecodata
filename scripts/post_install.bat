28e5a92fc8c22d0c3e44e9d6baf1d6ccb22df1cd
Madeline Scyphers <madelinescyphers@gmail.com>
2025-09-03 17:37:18 -0600

    Modify post_install.bat

--- scripts/post_install.bat
+++ scripts/post_install.bat
@@ -1,2 +1,3 @@
 call "%PREFIX%\Scripts\activate.bat"
-python -m pip install "git+https://github.com/%REPO%@%ECODATA_INSTALL_BRANCH%"
+python -m pip install git+https://github.com/%REPO%@%ECODATA_INSTALL_BRANCH%
+pause
